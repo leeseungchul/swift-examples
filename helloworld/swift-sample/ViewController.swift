0ed4822375f9eb2b7a8f2c09811faa99a79c7441
HyunJin Moon <arnold.spring@gmail.com>
2014-07-05 01:18:54 +0900

    helloworld 추가

---
+++ helloworld/swift-sample/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  swift-sample
+//
+//  Created by HyunJin Moon on 2014. 7. 5..
+//  Copyright (c) 2014년 HyunJin Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var myLabel : UILabel;
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func myAction(sender:AnyObject){
+        myLabel.text = "Hello World";
+    }
+}
+
